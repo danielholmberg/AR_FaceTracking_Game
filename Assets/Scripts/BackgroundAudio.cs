d9ee099beb14943dc22bb74b696c7b8b6b54a2a9
Daniel Holmberg (Dev) <danielholmberg.dev@gmail.com>
2020-03-04 17:42:07 +0100

    Added background audio.

---
+++ Assets/Scripts/BackgroundAudio.cs
@@ -0,0 +1,10 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class BackgroundAudio : MonoBehaviour
+{
+    private void Awake() {
+        DontDestroyOnLoad(gameObject);
+    }
+}
